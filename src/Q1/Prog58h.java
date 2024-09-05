795a61157fdf847a46402f8bbb7f3e4c73bb3109
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-09-05 11:26:09 -0500

    IDK

---
+++ src/Q1/Prog58h.java
@@ -0,0 +1,10 @@
+package Q1;
+import java.util.*;
+
+public class Prog58h {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter amount saved: ");
+    }
+}

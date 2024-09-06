562c982d2c40cf566f9a09f560203045e1cbec69
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-09-06 11:25:43 -0500

    IDK

--- src/Q1/Prog58h.java
+++ src/Q1/Prog58h.java
@@ -6,5 +6,22 @@ public class Prog58h {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter amount saved: ");
+        int amount = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter the interest rate: ");
+        double rate = input.nextDouble();
+        System.out.println();
+
+        System.out.print("Enter number of times compounded per year: ");
+        int year = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter number of day at interest: ");
+        int days = input.nextInt();
+        System.out.println();
+
+        double interest = amount*(1+(0.1*rate/year)*year*days/365);
+        System.out.print(interest);
     }
 }

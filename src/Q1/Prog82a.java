18623dde6526f01e17cdd63038e0d56c79cc8279
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-09-18 11:37:51 -0500

    Update 9/18/2024

---
+++ src/Q1/Prog82a.java
@@ -0,0 +1,13 @@
+package Q1;
+import java.util.*;
+
+public class Prog82a {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the speed limit: ");
+        int a = input.nextInt();
+        System.out.println("Enter the vehicle speed: ");
+        int b = input.nextInt();
+    }
+}

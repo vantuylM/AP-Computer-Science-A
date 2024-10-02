f9df70399c1486c72673abd95d7185ed1ae1300c
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-02 11:37:18 -0500

    Update 10/2/2024

--- src/Q1/Prog122a.java
+++ src/Q1/Prog122a.java
@@ -1,4 +1,11 @@
 package Q1;
 
 public class Prog122a {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a <=50){
+            int Square = a * a;
+            double SquareRoot =
+        }
+    }
 }

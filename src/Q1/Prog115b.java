c43134a12cfa0aebc7a962f4c0148c1f50eebd86
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-02 11:14:16 -0500

    Update 10/2/2024

---
+++ src/Q1/Prog115b.java
@@ -0,0 +1,30 @@
+package Q1;
+
+public class Prog115b {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a <= 32){
+            a += 2;
+            System.out.println(a);
+        }
+    }
+}
+/*
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+ */

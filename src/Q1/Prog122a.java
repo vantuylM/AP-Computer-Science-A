c524c8ca3bae2d80f77032e76308fad672f3871d
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-07 11:25:32 -0500

    Update 10/7/2024

--- src/Q1/Prog122a.java
+++ src/Q1/Prog122a.java
@@ -2,10 +2,15 @@ package Q1;
 
 public class Prog122a {
     public static void main(String[] args) {
-        int a = 1;
-        while (a <=50){
-            int Square = a * a;
-            double SquareRoot =0;
+        int a = 1; /*"number \t\t square \t\t square root*/
+        int square = 0;
+        double SquareRoot = 0;
+        int Square = 0;
+        while (a <= 50) {
+            Square = a * a;
+            SquareRoot = Math.sqrt(a);
+            //System.out.print("number \t\t square \t\t square root \n" + a + "\t\t" + Square + "\t\t" + SquareRoot);
         }
+        System.out.println("\n" + a + "\t\t" + Square + "\t\t" + SquareRoot);
     }
 }

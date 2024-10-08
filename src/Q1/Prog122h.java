cf732600b87417f92290f1445991bc8c9cc67d32
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-08 11:25:39 -0500

    Update 10/8/2024

---
+++ src/Q1/Prog122h.java
@@ -0,0 +1,12 @@
+package Q1;
+
+public class Prog122h {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.print("Number\tSquare\tSquareRoot\tCube\tForthRoot");
+        while (a < 20) {
+            a ++;
+            System.out.print("\n"+a+"\t"+(a*a)+"\t"+(Math.sqrt(a))+"\t"+(a*a*a)+"\t"())
+        }
+    }
+}

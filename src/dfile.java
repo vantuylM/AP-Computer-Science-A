435dc3335a9ae57b9fad5e2fea1a5e33eb4e1f96
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-15 11:17:52 -0500

    Update 10/15/2024

---
+++ src/dfile.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class dfile {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/FILENAME"));
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getStackTrace());
+        }
+    }
+}

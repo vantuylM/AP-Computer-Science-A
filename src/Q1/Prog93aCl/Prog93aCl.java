da98ba9b8e96f4767ab1447fbe40abaf4254f8b9
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-15 11:25:21 -0500

    Update 10/15/2024

--- src/Q1/Prog93aCl/Prog93aCl.java
+++ src/Q1/Prog93aCl/Prog93aCl.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Prog93aCl {
     public static void main(String[] args) {
-        // TODO: use scanner for input; remove code below
         Scanner input = new Scanner(System.in);
         int kwh = input.nextInt();
     }

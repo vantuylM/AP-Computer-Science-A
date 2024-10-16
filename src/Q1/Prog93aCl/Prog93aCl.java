827fe307a84d3defc27c8e98891f1e2e93539543
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-16 11:24:12 -0500

    Update 10/16/2024

--- src/Q1/Prog93aCl/Prog93aCl.java
+++ src/Q1/Prog93aCl/Prog93aCl.java
@@ -1,9 +1,16 @@
 package Q1.Prog93aCl;
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Prog93aCl {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int kwh = input.nextInt();
+
+        new File("Langdat/Prog93a.dat");
+        Cl93a wow = new Cl93a(kwh);
+        wow.calc();
+        System.out.print(wow.toString());
     }
 }

4e405c4d9b37cc3f187d0b14a5112e3789a349a6
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-17 11:25:10 -0500

    Update 10/17/2024

---
+++ src/Q1/LP4_2.java
@@ -0,0 +1,11 @@
+package Q1;
+import java.util.*;
+
+public class LP4_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter weight in kilograms: ");
+        int kilograms = input.nextInt();
+
+    }
+}

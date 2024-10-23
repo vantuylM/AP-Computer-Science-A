6df1ab90160e783d68f67bac5db17583607a9da9
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-23 11:25:26 -0500

    Update 10/18/2024

--- src/Q1/LP4_3.java
+++ src/Q1/LP4_3.java
@@ -6,20 +6,29 @@ public class LP4_3 {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number of eggs: ");
         int eggs = input.nextInt();
-        int dozen = 0;
-        double price = 0;
-        double a = 0;
-        while (eggs >= 12) {
-            eggs -= 12;
-            dozen += 1;
-        }
+        int dozen = eggs / 12;
+        int remain = eggs % 12;
+        double price = 0.00;
+        double perEgg = 0;
+        double extraEgg = 0;
+
         if (dozen < 4) {
             price = 0.50;
-            a = (eggs) * (price * 0.0833;
-        }
-        if (dozen < 6 && dozen >= 4) {
+            perEgg = price / 12;
+            extraEgg = perEgg * remain;
+        }else if (dozen >=4 && dozen < 6) {
             price = 0.45;
-            a = (eggs) * (price * 0.833);
+            perEgg = price / 12;
+            extraEgg = perEgg * remain;
+        }else if (dozen >=6 && dozen < 11){
+            price = 0.40;
+            perEgg = price / 12;
+            extraEgg = perEgg * remain;
+        }else if (dozen >=11){
+            price = 0.35;
+            perEgg = price / 12;
+            extraEgg = perEgg * remain;
         }
+        System.out.print("The bill is: "+ bill);
     }
 }

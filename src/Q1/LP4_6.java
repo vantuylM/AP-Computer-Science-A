ea0d75b401ffa0332db1dbfa299aba3f47796193
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-28 11:24:54 -0500

    Update 10/28/2024

---
+++ src/Q1/LP4_6.java
@@ -0,0 +1,9 @@
+package Q1;
+import java.util.*;
+
+public class LP4_6 {
+    public static void main(String[] args) {
+        Random a = new Random();
+        int num1 = a.nextInt(10);
+    }
+    }

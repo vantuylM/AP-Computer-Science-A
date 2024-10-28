8e7d68ff4492f1f275021711d15868db39335840
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-28 10:41:02 -0500

    Update 10/28/2024

--- src/Q1/LP4_3.java
+++ src/Q1/LP4_3.java
@@ -13,22 +13,28 @@ public class LP4_3 {
         double extraEgg = 0;
 
         if (dozen < 4) {
-            price = 0.50;
+            price = 0.50 * dozen;
             perEgg = price / 12;
             extraEgg = perEgg * remain;
         }else if (dozen >=4 && dozen < 6) {
-            price = 0.45;
+            price = 0.45 * dozen;
             perEgg = price / 12;
             extraEgg = perEgg * remain;
         }else if (dozen >=6 && dozen < 11){
-            price = 0.40;
+            price = 0.40 * dozen;
             perEgg = price / 12;
             extraEgg = perEgg * remain;
         }else if (dozen >=11){
-            price = 0.35;
+            price = 0.35 * dozen;
             perEgg = price / 12;
             extraEgg = perEgg * remain;
         }
+        double bill = extraEgg + price;
         System.out.print("The bill is: "+ bill);
     }
 }
+/*
+Enter number of eggs:
+18
+The bill is: 0.75
+ */

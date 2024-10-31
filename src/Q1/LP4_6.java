c8f8fdcadec5499e2758ad6ee06492ea8cc89c60
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-10-31 10:57:55 -0500

    Update 10/31/2024

--- src/Q1/LP4_6.java
+++ src/Q1/LP4_6.java
@@ -3,11 +3,53 @@ import java.util.*;
 
 public class LP4_6 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Random a = new Random();
         Random b = new Random();
-        int num2 = b.nextInt();
+        Random c = new Random();
+        int num3 = c.nextInt(10);
+        int num2 = b.nextInt(10);
         int num1 = a.nextInt(10);
+        String x = "";
+        double answer = 0;
+        String y = "";
 
-        //if (num3)
+        if (num3<=3){
+            x += "*";
+            answer = num1 * num2;
+            y = num1 + x + num2;
+        }
+        else if (num3>3 && num3<=6){
+            x += "+";
+            answer = num1 + num2;
+            y = num1 + x + num2;
+        }
+        else if (num3>6 && num3<=8){
+            x += "-";
+            answer = num1 - num2;
+            y = num1 + x + num2;
+        }
+        else if (num3>8 && num3<=10){
+            x += "/";
+            answer = num1 / num2;
+            y = num1 + x + num2;
+        }
+        System.out.println(y + ":");
+        double in = input.nextDouble();
+        if (answer == in){
+            System.out.print("Correct!");
+        }else {
+            System.out.print("Incorrect!");
+        }
     }
     }
+
+    /*
+    8+3:
+55
+Incorrect!
+
+1-8:
+-7
+Correct!
+     */

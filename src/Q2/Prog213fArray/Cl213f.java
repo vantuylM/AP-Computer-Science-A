95e993173339262493ab7304f5d2c887d6aee7a5
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-11-06 10:58:00 -0600

    Update 11/5/2024

---
+++ src/Q2/Prog213fArray/Cl213f.java
@@ -0,0 +1,18 @@
+package Q2.Prog213fArray;
+
+public class Cl213f {
+    private double myKwh;
+    private double myCost;
+
+    public Cl213f(double Kwh) {
+        myKwh = Kwh;
+        myCost = 0;
+    }
+
+    public void calc() {//TODO: complete
+
+    }
+    public String toString() {
+        return "The cost of "; // TODO: complete
+    }
+}
---
+++ src/Q2/Prog213fArray/Prog213fArray.java
@@ -0,0 +1,34 @@
+package Q2.Prog213fArray;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog213fArray {
+public static void main(String[] args) {
+    try {
+        Scanner file = new Scanner(new File("Langdat/prog213f.dat"));
+
+        Cl213f[] list = new Cl213f[1000];
+        int cnt = 0;
+
+        while (file.hasNext()){
+            int Kwh = file.nextInt();
+
+            if (Kwh != -999) {
+                Cl213f yikes = new Cl213f(Kwh);
+                list[cnt] = yikes;
+                cnt++;
+            }
+        }
+
+        for (int lcv = 0; lcv < cnt; lcv++) {
+            list[lcv].calc();
+            System.out.println(list[lcv]); // list[lcv].toString()
+        }
+
+    } catch (IOException e) {
+        System.out.println("Error: " + e.getStackTrace());
+    }
+}
+}

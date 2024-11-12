886714122b22e1885406975ef987292534dd0bd4
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-11-12 11:23:03 -0600

    Update 11/12/2024

--- src/Q2/BigArray.java
+++ src/Q2/BigArray.java
@@ -60,5 +60,75 @@ public class BigArray {
         int myThird = array[2];
         array[2] = 5;
         System.out.println("The number that was ousted is " + myThird);
+
+        for (int num : array)
+            if (num >= 50 && num <= 59)
+                System.out.print(num + " ");
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++)
+            if (array[i] % 4 == 0)
+                System.out.print(array[i] + " ");
+        System.out.println();
+
+        boolean sixty = false;
+        for (int n : array)
+            if (n == 60) sixty = true;
+        System.out.println("Is 60 in the list: " + sixty);
+
+        boolean same = true;
+        for (int lcv = 0; lcv < array.length; lcv++)
+            if (array[lcv] != array[(array.length-1) - lcv])
+                same = false;
+        System.out.println("Is the array palindromic: " + same);
+
+        double average = 0;
+        for (int x : array) average += x;
+        average /= array.length;
+
+        int avgCount = 0;
+        for (int x : array)
+            if (x > average) avgCount ++;
+        System.out.printf("There are %d numbers greater than the average\n", avgCount);
+
+        int evens = 0;
+        for (int num : array)
+            if (num % 2 == 0) evens++;
+        System.out.printf("There are %d even numbers\n",evens);
+
+        int[] array2 = new int[array.length];
+        for (int i = 0; i < array.length; i++)
+            array2[i] = array[(array.length-1) - i];
+
+        for ( int n : array2)
+            System.out.print(n + " ");
+        System.out.println();
+
+        int[] shiftArr = circularShiftRight(array);
+        for (int n : shiftArr)
+            System.out.print(n + " ");
+        System.out.println();
+
+        int sum = 0;
+        for (int num : array) {
+            int digitsum = 0;
+            int temp = num;
+            while (temp > 0) {
+                int n = temp % 10;
+                temp /= 10;
+                digitsum += n;
+            }
+            sum += digitsum;
+        }
+        System.out.println("Sum of all digits of all elements: " + sum);
+    }
+
+    public static int[] circularShiftRight(int[] arr) {
+        int temp = arr[arr.length - 1];
+        int[] shifted = new int[arr.length];
+        for (int lcv = 1; lcv < arr.length; lcv++)
+            shifted[lcv] = arr[lcv-1];
+        shifted[0] = temp;
+        return shifted;
     }
 }

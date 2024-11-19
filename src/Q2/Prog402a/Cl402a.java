fbde6ea74fa2b82e6186f9919d26b6d05d5f685e
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-11-19 11:23:12 -0600

    Update 11/19/2024

--- src/Q2/Prog402a/Cl402a.java
+++ src/Q2/Prog402a/Cl402a.java
@@ -4,16 +4,18 @@ public class Cl402a {
     private int myId;
     private int myScore;
     private double myDiff;
+    private double myAverage;
 
     public Cl402a(int id, int score){
         myId = id;
         myScore = score;
         myDiff = 0;
+        myAverage = 0;
     }
 
     public void calc(){
-        double average = myScore / 21;
-               myDiff = myScore - average;
+        myAverage = myScore / 21;
+        myDiff = myScore - myAverage;
     }
 
     public String toString(){

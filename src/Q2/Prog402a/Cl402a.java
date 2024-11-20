bfab1478fd0c44510243434aa599d317852ca24b
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-11-20 11:35:29 -0600

    Update 11/20/2024

--- src/Q2/Prog402a/Cl402a.java
+++ src/Q2/Prog402a/Cl402a.java
@@ -5,16 +5,18 @@ public class Cl402a {
     private int myScore;
     private double myDiff;
     private double myAverage;
+    private double myTotal;
 
-    public Cl402a(int id, int score){
+    public Cl402a(int id, int score, double total){
         myId = id;
         myScore = score;
         myDiff = 0;
         myAverage = 0;
+        myTotal = total;
     }
 
     public void calc(){
-        myAverage = myScore / 21;
+        myAverage = myTotal / 21;
         myDiff = myScore - myAverage;
     }
 
--- src/Q2/Prog402a/Prog402a.java
+++ src/Q2/Prog402a/Prog402a.java
@@ -8,13 +8,16 @@ public class Prog402a {
     public static void main(String[] args) {;
     try {
         Scanner file = new Scanner(new File("Langdat/p402a.dat"));
+        double total = 0;
 
         while (file.hasNext()) {
             int id = file.nextInt();
             int score = file.nextInt();
-            Cl402a wow = new Cl402a(id, score);
+            total += score;
+            System.out.println(total);
+            Cl402a wow = new Cl402a(id, score, total);
             wow.calc();
-            System.out.println(wow.toString());
+            System.out.println(wow);
         }
 
     } catch (IOException e) {

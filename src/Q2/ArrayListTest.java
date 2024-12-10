f39608ec94f4f8dbdd0f10272e6b3294e64c9258
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-12-10 10:36:08 -0600

    Update 12/9/2024

---
+++ src/Q2/ArrayListTest.java
@@ -0,0 +1,29 @@
+package Q2;
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        // Dynamic array/list operations: add, remove, get, set
+        // add(obj), add(index, obj) -- insert, remove(index), get, set(index, obj)
+        ArrayList<Integer> list = new ArrayList <Integer>();
+        // ArrayList<WrapperType> for primitive types
+
+        for (int i = 0; i < 100; i++) {
+            int rand = (int) (Math.random() * 100) + 1;
+            list.add(rand);
+        }
+
+        for (int i = 0; i < list.size(); i += 2)
+            System.out.print(list.get(i) + " ");
+        System.out.println();
+
+        System.out.println("Does list contain 50! " + list.contains(50));
+        System.out.println("Index of 50: " + list.indexOf(50));
+
+        list.add(2, 999);
+        list.set(0, 123);
+
+        for (int num : list)
+            System.out.println(num);
+    }
+}
--- src/Q2/LP3_10/LP3_10.java
+++ src/Q2/LP3_10/LP3_10.java
@@ -1,6 +1,20 @@
 package Q2.LP3_10;
-import java.util.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class LP3_10 {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/FILENAME"));
 
+            while (file.hasNext()) {
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getStackTrace());
+        }
+    }
 }

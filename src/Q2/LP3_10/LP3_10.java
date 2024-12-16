afce80928882733700110fcb229688fc1169787c
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2024-12-16 11:25:03 -0600

    Update 12/16/2024

--- src/Q2/LP3_10/LP3_10.java
+++ src/Q2/LP3_10/LP3_10.java
@@ -13,14 +13,24 @@ public class LP3_10 {
         int soda = input.nextInt();
         ClLP3_10 wow = new ClLP3_10(burger, fries, soda);
         wow.calc();
-        System.out.println("Before tax: " + wow.getTotal());
-        System.out.println("Tax: " + wow.getTax());
-        System.out.println("Final price: " + wow.getTaxTotal());
+        System.out.printf("Before tax: %.2f\n", wow.getTotal());
+        System.out.printf("Tax: %.2f\n", wow.getTax());
+        System.out.printf("Final price: %.2f\n", wow.getTaxTotal());
         System.out.print("Enter amount given: ");
         double given = input.nextDouble();
         //ClLP3_10 wow = new ClLP3_10(given);
         wow.setGiven(given);
         wow.calc();
-        System.out.print("Change: " + wow.getChange());
+        System.out.printf("Change: %.2f", wow.getChange());
     }
 }
+/*
+Enter number of burgers:2
+Enter number of fries:5
+Enter number of sodas:5
+Before tax: 13.78
+Tax: 0.90
+Final price: 14.68
+Enter amount given: 20
+Change: 5.32
+ */

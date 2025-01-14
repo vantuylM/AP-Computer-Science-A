30ffbcf8ddaa00d394d5b2c94c640af7e885c0c2
vantuyl.m <vantuyl.m@stu.janesville.k12.wi.us>
2025-01-14 12:14:43 -0600

    Update 1/14/2025

---
+++ src/Q2/Prog465b.java
@@ -0,0 +1,36 @@
+package Q2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Prog465b {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/prog465b.dat"));
+            int[][] mat = new int[3][4];
+
+            for (int r = 0; r < mat.length; r++)
+                for (int c = 0; c < mat[r].length; c++)
+                    mat[r][c] = file.nextInt();
+
+            System.out.println("Original Table:");
+
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[r].length; c++)
+                    System.out.print(mat[r][c] + " ");
+                System.out.println();
+            }
+
+            System.out.println();
+
+            for (int r = 0; r < mat.length; r++) {
+                for (int c = 0; c < mat[r].length;)
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getStackTrace());
+        }
+    }
+}
